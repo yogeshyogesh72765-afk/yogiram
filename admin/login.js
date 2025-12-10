93c1bbd6e38357b193b44bfc908e24f7eeceb676
yogeshyogesh72765-afk <yogeshyogesh72765@gmail.com>
2025-12-10 16:50:03 +0530

    Update login.js

--- admin/login.js
+++ admin/login.js
@@ -3,6 +3,6 @@ document.getElementById('loginForm').addEventListener('submit', function(e){
   e.preventDefault();
   const u = document.getElementById('user').value;
   const p = document.getElementById('pass').value;
-  if(u==='admin' && p==='123456'){ localStorage.setItem('admin','1'); location.href='panel.html'; }
+  if(u==='arvind' && p==='babU@5962'){ localStorage.setItem('admin','1'); location.href='panel.html'; }
   else alert('Wrong credentials');
 });
